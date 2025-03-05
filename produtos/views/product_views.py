ae8b4de079555e2bfbc19d36eff013426b5827ad
marcusguelfi <marcusguelfi@gmail.com>
2025-03-05 13:43:24 -0300

    redirect invalid acess

--- produtos/views/product_views.py
+++ produtos/views/product_views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 from ..models import Produto
 from ..forms import ProdutoForm
 
-@login_required
+@login_required(login_url='login')
 def home(request):
     query = request.GET.get('q', '')
     if query:
@@ -15,7 +15,7 @@ def home(request):
     return render(request, 'home.html', {'produtos': produtos, 'query': query})
 
 
-@login_required
+@login_required(login_url='login')
 def adicionar_produto(request):
     if request.method == 'POST':
         form = ProdutoForm(request.POST)
@@ -28,7 +28,7 @@ def adicionar_produto(request):
         form = ProdutoForm()
     return render(request, 'adicionar_produto.html', {'form': form})
 
-@login_required
+@login_required(login_url='login')
 def editar_produto(request, id):
     produto = get_object_or_404(Produto, id=id)
     
@@ -41,14 +41,14 @@ def editar_produto(request, id):
         form = ProdutoForm(instance=produto)
     return render(request, 'editar_produto.html', {'form': form})
 
-@login_required
+@login_required(login_url='login')
 def excluir_produto(request, id):
     produto = get_object_or_404(Produto, id=id)
     if request.user.has_perm('produtos.delete_produto') or request.user.groups.filter(name='Supervisor').exists():
         produto.delete()
     return redirect('home')
 
-@login_required
+@login_required(login_url='login')
 def buscar_produtos(request):
     query = request.GET.get('q', '')
     produtos = Produto.objects.filter(nome__icontains=query) if query else Produto.objects.all()  
