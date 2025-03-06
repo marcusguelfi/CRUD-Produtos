a7fb676d5f4a4e82e7ee756b26d733545722ecd8
marcusguelfi <marcusguelfi@gmail.com>
2025-03-05 21:06:53 -0300

    fix permissions and add messages in home page

--- produtos/views/product_views.py
+++ produtos/views/product_views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.contrib import messages
 from ..models import Produto
 from ..forms import ProdutoForm
+
+
+def _produtos_filtrados(q):
+    return Produto.objects.filter(nome__icontains=q) if q else Produto.objects.all()  
+
 
 @login_required(login_url='login')
 def home(request):
     query = request.GET.get('q', '')
-    if query:
-        produtos = Produto.objects.filter(nome__icontains=query)
-    else:
-        produtos = Produto.objects.all()
-    
+    produtos = _produtos_filtrados(query)
     return render(request, 'home.html', {'produtos': produtos, 'query': query})
 
 
@@ -23,35 +25,44 @@ def adicionar_produto(request):
             produto = form.save(commit=False)
             produto.usuario = request.user
             produto.save()
-            return redirect('home')
+            messages.success(request, 'Produto adicionado com sucesso!')
+            return redirect('home')  
     else:
         form = ProdutoForm()
     return render(request, 'adicionar_produto.html', {'form': form})
 
+
 @login_required(login_url='login')
 def editar_produto(request, id):
     produto = get_object_or_404(Produto, id=id)
-    
-    if request.method == 'POST':
+
+    if request.method == 'POST' and request.user.has_perm('produtos.change_produto'):
         form = ProdutoForm(request.POST, instance=produto)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            messages.success(request, 'Produto atualizado com sucesso!')
+            return redirect('home') 
     else:
         form = ProdutoForm(instance=produto)
     return render(request, 'editar_produto.html', {'form': form})
 
+
 @login_required(login_url='login')
 def excluir_produto(request, id):
     produto = get_object_or_404(Produto, id=id)
+    
     if request.user.has_perm('produtos.delete_produto') or request.user.groups.filter(name='Supervisor').exists():
         produto.delete()
-    return redirect('home')
+        messages.success(request, 'Produto excluído com sucesso!')
+    else:
+        messages.error(request, 'Você não tem permissão para excluir produtos.')
+    
+    return redirect('home')  
+
 
 @login_required(login_url='login')
 def buscar_produtos(request):
-    query = request.GET.get('q', '')
-    produtos = Produto.objects.filter(nome__icontains=query) if query else Produto.objects.all()  
-    produtos_json = list(produtos.values("nome", "descricao", "preco", "quantidade"))
-    return JsonResponse(produtos_json, safe=False)  
-
+    produtos_list = list(
+        _produtos_filtrados(request.GET.get('q', '')).values("nome", "descricao", "preco", "quantidade")
+    )
+    return JsonResponse(produtos_list, safe=False)
